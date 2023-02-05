e0adbb829835c61b77c00d3f9c8fe0086c0dbd4c
jchapar <chaparrojcjr@gmail.com>
2023-02-05 16:56:27 -0700

    mobile header - navbar initial markup and styling classes done for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1440px',
+    },
+    extend: {
+      colors: {
+        primary: '#Fc4747',
+        steel: '#5a698f',
+        dark: '#10141e',
+        navy: '#161d2f',
+        white: '#ffffff',
+      },
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
