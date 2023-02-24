971984b4168760e307e0ba0fda7793dc8f858260
jchapar <chaparrojcjr@gmail.com>
2023-02-23 22:19:30 -0700

    data being fetched and shown for the recommended section

--- script.js
+++ script.js
@@ -1,42 +1,85 @@
-async function fetchData() {
-  const response = await fetch('../data.json')
+// Fetching Trending Data============================================================================================================
+async function fetchTrendingData() {
+  const response = await fetch('./data.json')
 
   const data = await response.json()
 
   console.log(data)
 
   data.forEach((item) => {
-    const div = document.createElement('div')
-    div.classList.add('swiper-slide')
+    if (item.isTrending === true) {
+      const div = document.createElement('div')
+      div.classList.add('swiper-slide')
 
-    div.innerHTML = `
-    <div class="swiper-img">
-    <img src="${item.thumbnail.trending.large}" alt="${item.title}">
-  </div>
-  <div class="content">
-    <div class="details">
-      <span class="year">${item.year}</span>
-      <span class="dot-one">&#8226</span>
-      <span class="genre"
-        ><img src="assets/icon-category-${item.category}.svg" alt="${item.category}"
-      /><p>${item.category}</p></span>
-      <span class="dot-two">&#8226</span>
-      <span class="rating">${item.rating}</span>
+      div.innerHTML = `
+      <div class="swiper-img">
+      <img src="${item.thumbnail.trending.large}" alt="${item.title}">
     </div>
-    <div class="title">${item.title}</div>
-  </div>
-  <div class="rating-badge">
-    <p class="rating">${item.rating}</p>
-  </div>
-    `
+    <div class="content">
+      <div class="details">
+        <span class="year">${item.year}</span>
+        <span class="dot-one">&#8226</span>
+        <span class="genre"
+          ><img src="assets/icon-category-${item.category}.svg" alt="${item.category}"
+        /><p>${item.category}</p></span>
+        <span class="dot-two">&#8226</span>
+        <span class="rating">${item.rating}</span>
+      </div>
+      <div class="title">${item.title}</div>
+    </div>
+    <div class="rating-badge">
+      <p class="rating">${item.rating}</p>
+    </div>
+      `
 
-    document.querySelector('.swiper-wrapper').appendChild(div)
+      document.querySelector('.swiper-wrapper').appendChild(div)
 
-    initSwiper()
+      initSwiper()
+    }
   })
 }
 
-fetchData()
+// Fetching Recommended Data============================================================================================================
+async function fetchRecommendedData() {
+  const response = await fetch('./data.json')
+
+  const data = await response.json()
+
+  console.log(data)
+
+  data.forEach((item) => {
+    if (item.isTrending === false) {
+      const div = document.createElement('div')
+      div.classList.add('grid-item')
+
+      div.innerHTML = `
+
+      <div class="movie-img">
+        <img src="${item.thumbnail.regular.large}" alt="${item.title}" class="movie-poster">
+        <div class="bookmark-wrapper">
+          <img src="assets/icon-bookmark-empty.svg" alt="Bookmark Empty" class="bookmark">
+        </div>
+      </div>
+      <div class="movie-details">
+        <span class="year">${item.year}</span>
+        <span class="dot-one">&#8226</span>
+        <span class="genre"
+          ><img src="assets/icon-category-${item.category}.svg" alt="${item.category}"
+        /><p>${item.category}</p></span>
+        <span class="dot-two">&#8226</span>
+        <span class="rating">${item.rating}</span>
+      </div>
+      <div class="movie-title">
+        <h3>${item.title}</h3>
+      </div>
+
+      `
+
+      document.querySelector('.grid-container').appendChild(div)
+    }
+  })
+}
+
 // Init Swiper ============================================================================================================
 function initSwiper() {
   const swiper = new Swiper('.swiper', {
@@ -61,3 +104,13 @@ function initSwiper() {
     },
   })
 }
+
+// Init App ============================================================================================================
+function init() {
+  // Fetching Trending Data
+  fetchTrendingData()
+  // Fetching Recommended Data
+  fetchRecommendedData()
+}
+
+document.addEventListener('DOMContentLoaded', init)
